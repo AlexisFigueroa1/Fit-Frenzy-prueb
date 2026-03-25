94daf25c7c3acc6c006c8c5d357a8dbb05675122
AlexisFigueroa1 <0324108274@ut-tijuana.edu.mx>
2026-03-25 13:00:39 -0700

    Set up game initialization and rendering functions
    Initialize game canvas and player, and set up game loop functions.

---
+++ game.js
@@ -0,0 +1,29 @@
+let canvas, ctx;
+let player;
+let objects = [];
+
+function initGame() {
+    canvas = document.getElementById("gameCanvas");
+    ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    player = new Player(canvas.width, canvas.height);
+}
+
+function spawnObject() {
+    let type = Math.random() < 0.7 ? "good" : "bad";
+    objects.push(new FallingObj(type, canvas.width));
+}
+
+function updateGame() {
+    objects.forEach(obj => obj.update());
+}
+
+function drawGame() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    player.draw(ctx);
+    objects.forEach(obj => obj.draw(ctx));
+}
