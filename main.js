a51d485d91233426c77be3e16ee345c3cb2a46e5
AlexisFigueroa1 <0324108274@ut-tijuana.edu.mx>
2026-03-25 13:01:01 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+let keys = {};
+
+window.addEventListener("keydown", e => keys[e.key] = true);
+window.addEventListener("keyup", e => keys[e.key] = false);
+
+function gameLoop() {
+    if(keys["ArrowLeft"]) player.move("left");
+    if(keys["ArrowRight"]) player.move("right");
+
+    updateGame();
+    drawGame();
+    updateUI(player);
+
+    requestAnimationFrame(gameLoop);
+}
+
+window.onload = () => {
+    initGame();
+    setInterval(spawnObject, 1000);
+    gameLoop();
+};
