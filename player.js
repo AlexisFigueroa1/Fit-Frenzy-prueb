f873c9c36b54e6f063f1fb075a492e55ca331f88
AlexisFigueroa1 <0324108274@ut-tijuana.edu.mx>
2026-03-25 13:05:51 -0700

    Remove health and update player color to cyan
    Removed health property and changed player color.

--- player.js
+++ player.js
@@ -6,16 +6,15 @@ class Player {
         this.y = canvasHeight - 60;
         this.speed = 8;
         this.score = 0;
-        this.health = 100;
     }
 
     move(dir) {
-        if(dir === 'left') this.x -= this.speed;
-        if(dir === 'right') this.x += this.speed;
+        if(dir === "left") this.x -= this.speed;
+        if(dir === "right") this.x += this.speed;
     }
 
     draw(ctx) {
-        ctx.fillStyle = "blue";
+        ctx.fillStyle = "cyan";
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
