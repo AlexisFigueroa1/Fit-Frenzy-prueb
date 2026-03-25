92409da9f4a8729252784d82ae55e6f1a517b474
AlexisFigueroa1 <0324108274@ut-tijuana.edu.mx>
2026-03-25 13:04:54 -0700

    Refactor GameItems structure in config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const GameItems = {
     good: [
-        {id:'creatina',name:'Creatina',color:'#d878d8',value:25,shape:'diamond'},
-        {id:'proteina',name:'Proteína',color:'#40b4dc',value:20,shape:'circle'}
+        {color:'green', value:10},
+        {color:'blue', value:15}
     ],
     bad: [
-        {id:'pizza',name:'Pizza',color:'#dc4646',value:-22,shape:'triangle'}
+        {color:'red', value:-20}
     ]
 };
